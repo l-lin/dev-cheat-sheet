897fad3a26e024351f4bcecd69426c463d4ce677
Louis LIN <louis.lin.87@gmail.com>
2014-05-15 00:06:59 +0200

    :1234: Add PriorityTasks

---
+++ java/src/main/java/exercice/prioritytasks/README.md
@@ -0,0 +1,4 @@
+Order tasks by priority
+=======================
+
+Given a list of tasks, write a program that will sort the list by their priority.
---
+++ java/src/test/java/lin/louis/exercice/prioritytasks/PriorityTasksTest.java
@@ -0,0 +1,31 @@
+package lin.louis.exercice.prioritytasks;
+
+import exercice.prioritytasks.PriorityTask;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author llin
+ * @created 14/05/14.
+ */
+public class PriorityTasksTest {
+    @Test
+    public void testSortTasks() {
+        List<PriorityTask.Task> taskList = newArrayList(
+                new PriorityTask.Task(4),
+                new PriorityTask.Task(2),
+                new PriorityTask.Task(10),
+                new PriorityTask.Task(5)
+        );
+
+        taskList = PriorityTask.orderTasks(taskList);
+        assertThat(taskList.get(0).getPriority()).isEqualTo(2);
+        assertThat(taskList.get(1).getPriority()).isEqualTo(4);
+        assertThat(taskList.get(2).getPriority()).isEqualTo(5);
+        assertThat(taskList.get(3).getPriority()).isEqualTo(10);
+    }
+}
