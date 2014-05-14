29c6ac2119b5bf3f2d46184ec9515c4ce67f695e
Louis LIN <louis.lin.87@gmail.com>
2014-05-14 23:10:49 +0200

    :muscle: Add Armstrong exercice

---
+++ java/src/test/java/lin/louis/exercice/armstrong/ArmstrongTest.java
@@ -0,0 +1,22 @@
+package lin.louis.exercice.armstrong;
+
+import exercice.armstrong.Armstrong;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author llin
+ * @created 14/05/14.
+ */
+public class ArmstrongTest {
+    @Test
+    public void testArmstrong() {
+        assertThat(Armstrong.isArmstrong(153)).isTrue();
+        assertThat(Armstrong.isArmstrong(371)).isTrue();
+        assertThat(Armstrong.isArmstrong(123)).isFalse();
+        assertThat(Armstrong.isArmstrong(1)).isFalse();
+        assertThat(Armstrong.isArmstrong(12)).isFalse();
+        assertThat(Armstrong.isArmstrong(1234)).isFalse();
+    }
+}
