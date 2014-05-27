268177083e80f382329d2e97a3dc0886c22d7482
Louis LIN <louis.lin.87@gmail.com>
2014-05-27 17:55:36 +0200

    Add sed + Read file in classpath

---
+++ java/src/test/java/lin/louis/file/ReadFileInClasspath.java
@@ -0,0 +1,27 @@
+package lin.louis.file;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Oodrive
+ * @author llin
+ * @created 27/05/14 17:43
+ */
+public class ReadFileInClasspath {
+    @Test
+    public void readFile() throws IOException {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("file/foo.txt");
+        assertThat(inputStream).isNotNull();
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(inputStream, writer, "UTF-8");
+
+        assertThat(writer.toString()).isEqualTo("foobar");
+    }
+}
