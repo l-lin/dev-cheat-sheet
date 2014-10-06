cba8611951462bfc471cff11b95ed3a3f1d17926
Louis LIN <louis.lin.87@gmail.com>
2014-10-06 18:08:44 +0200

    Add append to file

--- java/src/test/java/lin/louis/file/WriteToFile.java
+++ java/src/test/java/lin/louis/file/WriteToFile.java
@@ -6,6 +6,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +26,7 @@ public class WriteToFile {
         List<String> content = new ArrayList<>();
         content.add("foo");
         content.add("bar");
-        Files.write(path, content, Charset.defaultCharset());
+        Files.write(path, content, Charset.defaultCharset(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
 
         File file = new File("/home/llin/tmp/foobar.txt");
         assertThat(file.exists()).isTrue();
