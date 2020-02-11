33448a6b2040dce0cc08f3487db1f26c24880049
Louis Lin <lin.louis@pm.me>
2020-02-11 18:28:41 +0100

    shell: better flag usage in script-template.sh

--- shell/script-template.sh
+++ shell/script-template.sh
@@ -6,42 +6,62 @@
 set -e
 
 show_help() {
-cat << EOF
+  cat << EOF
 Usage: ${0##*/} <flags> <args>
     -h, --help             Display help
     -g, --greet            Greet the world
+    -v, --verbose          Verbose
 EOF
 }
 
 greet_user() {
-  echo "Hello, world"
+  local name="${1-World}"
+  echo "Hello, ${name}"
 }
 
 main() {
-    local greet=
-    while :; do
-        case "${1:-}" in
-            -h|--help)
-                show_help
-                exit
-                ;;
-            -g|--greet)
-                greet=true
-                ;;
-            -?*)
-                printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
-                ;;
-            *)
-                break
-                ;;
-        esac
-
+  local greet=
+  local verbose=false
+  # Flags in bash tutorial here: /usr/share/doc/util-linux/examples/getopt-parse.bash
+  TEMP=$(getopt -o 'hg:v' --long 'help,greet:,v' -n 'script-template.sh' -- "$@")
+  eval set -- "$TEMP"
+  unset TEMP
+  while true; do
+    case "${1}" in
+      '-h'|'--help')
+        show_help
+        exit
+        ;;
+      '-g'|'--greet')
+        greet="${2}"
+        shift 2
+        continue
+        ;;
+      '-v'|'--verbose')
+        verbose=true
+        echo "Verbose mode: ${verbose}"
         shift
-    done
+        continue
+        ;;
+      '--')
+        shift
+        break
+        ;;
+      *)
+        break
+        ;;
+    esac
 
-    if [[ -n "$greet" ]]; then
-        greet_user
-    fi
+    shift
+  done
+
+  if [[ -n "${greet}" ]]; then
+    greet_user "${greet}"
+  fi
+
+  for arg; do
+    echo "--> '$arg'"
+  done
 }
 
 main "$@"
