8043404d5773392bb1db1b192b4e93c368503bc7
Louis Lin <lin.louis@pm.me>
2020-02-12 15:37:01 +0100

    shell: improve script-template.sh to include arguments + read + env variables

--- shell/script-template.sh
+++ shell/script-template.sh
@@ -5,25 +5,53 @@
 
 set -e
 
+foo=${FOO:-foo}
+
 show_help() {
   cat << EOF
+Script description here
+
 Usage: ${0##*/} <flags> <args>
-    -h, --help             Display help
-    -g, --greet            Greet the world
-    -v, --verbose          Verbose
+
+Examples:
+    # Prompt name when greeting
+      ${0##*/} greet
+    # Greet Louis without prompting
+      ${0##*/} greet --name Louis
+    # Use foo
+      FOO=foobar ${0##*/} foo
+    # Use bar
+      ${0##*/} bar azerty
+
+Available commands:
+    greet                 Greet someone
+    foo                   Display 'foo'
+    bar                   Display 'bar'
+
+Flags:
+    -h, --help            Display help
+    -n, --name            Name of the person to greet
+    -v, --verbose         Verbose
+
+Environment variables:
+    FOO                   Some environment variable to set
+
 EOF
 }
 
 greet_user() {
-  local name="${1-World}"
+  local name="${1}"
+  if [[ -z "${name}" ]]; then
+    read -p "person to greet: " -r name
+  fi
   echo "Hello, ${name}"
 }
 
 main() {
-  local greet=
+  local name=
   local verbose=false
   # Flags in bash tutorial here: /usr/share/doc/util-linux/examples/getopt-parse.bash
-  TEMP=$(getopt -o 'hg:v' --long 'help,greet:,v' -n 'script-template.sh' -- "$@")
+  TEMP=$(getopt -o 'hn:v' --long 'help,name:,v' -n 'script-template.sh' -- "$@")
   eval set -- "$TEMP"
   unset TEMP
   while true; do
@@ -32,8 +60,8 @@ main() {
         show_help
         exit
         ;;
-      '-g'|'--greet')
-        greet="${2}"
+      '-n'|'--name')
+        name="${2}"
         shift 2
         continue
         ;;
@@ -55,13 +83,20 @@ main() {
     shift
   done
 
-  if [[ -n "${greet}" ]]; then
-    greet_user "${greet}"
-  fi
-
-  for arg; do
-    echo "--> '$arg'"
-  done
+  case "${1}" in
+    'greet')
+      greet_user "${name}"
+      ;;
+    'foo')
+      echo "foo ${foo}"
+      ;;
+    'bar')
+      echo "bar ${2}"
+      ;;
+    *)
+      show_help
+      ;;
+  esac
 }
 
 main "$@"
