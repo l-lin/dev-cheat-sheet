b9701ea44575a8e445ff0c4543dc6fcd0475c47f
Louis Lin <lin.louis@pm.me>
2020-02-24 10:37:57 +0100

    go: fix bubblesort

--- golang/exercises/bubblesort/bubblesort.go
+++ golang/exercises/bubblesort/bubblesort.go
@@ -1,11 +1,17 @@
-package bubblesort
+package main
 
-func BubbleSort(arr []int) {
-	for i := 0; i < len(arr) - 1; i++ {
-		for j := i + 1; j < len(arr); j++ {
-			if arr[i] > arr[j] {
-				arr[i], arr[j] = arr[j], arr[i]
+import "log"
+
+func bubbleSort(arr []int) []int {
+	sorted := make([]int, len(arr))
+	copy(sorted, arr)
+	log.Println(sorted)
+	for i := 0; i < len(sorted)-1; i++ {
+		for j := i + 1; j < len(sorted); j++ {
+			if sorted[i] > sorted[j] {
+				sorted[i], sorted[j] = sorted[j], sorted[i]
 			}
 		}
 	}
+	return sorted
 }
--- golang/exercises/bubblesort/bubblesort_test.go
+++ golang/exercises/bubblesort/bubblesort_test.go
@@ -1,12 +1,31 @@
-package bubblesort
+package main
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestBubbleSort(t *testing.T) {
-	arr := []int{2, 3, 1}
-	BubbleSort(arr)
-	fmt.Printf("%v\n", arr)
+	var tests = map[string]struct {
+		given    []int
+		expected []int
+	}{
+		"sort": {
+			given:    []int{3, 1, 2, 5, 8, 4},
+			expected: []int{1, 2, 3, 4, 5, 8},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual := bubbleSort(tt.given)
+			if len(actual) != len(tt.expected) {
+				t.Errorf("expected length %d, actual length %d", len(tt.expected), len(actual))
+			} else {
+				for i := 0; i < len(actual); i++ {
+					if actual[i] != tt.expected[i] {
+						t.Errorf("%d: expected %d, actual %d", i, tt.expected[i], actual[i])
+					}
+				}
+			}
+		})
+	}
 }
